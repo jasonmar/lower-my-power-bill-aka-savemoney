eb04f7ca2a9ed167a94ec24b5f4de20126229a33
Jason Mar <jasonmar@gmail.com>
2013-11-10 01:55:44 +0000

    Add landing page, suggested Actions, DR2 rate analysis

--- app/models/Usage.scala
+++ app/models/Usage.scala
@@ -63,10 +63,10 @@ case class Usage (custID:Int) {
 
     /* Baselines by Region */
 
-    val baselineCoastal = (294,498)
-    val baselineInland = (330,549)
-    val baselineMountain = (519,855)
-    val baselineDesert = (585,660)
+    val baselineCoastal = List(294,498)
+    val baselineInland = List(330,549)
+    val baselineMountain = List(519,855)
+    val baselineDesert = List(585,660)
 
     /* prices in cents per kWh */
 
@@ -95,6 +95,57 @@ case class Usage (custID:Int) {
       val monthData = monthIDs.map(id => data(id).toList)
       monthData
     }
+	
+	def getMonthUsage(monthID:Int) = {
+		val monthData = getMonthData(monthID)
+		val monthUsage = monthData.map(_.sum).sum
+		monthUsage
+	}
+	
+	def getMonthlyBillDR2(monthUsage:Int) = {
+		val t1max = (baselineCoastal(1)).toInt
+		val t2max = (baselineCoastal(1)*1.3).toInt
+		val t3max = (baselineCoastal(1)*2).toInt
+	
+		val t1Usage = monthUsage match {
+			case x if x > t1max => t1max
+			case _ => monthUsage
+		}
+		val t2Usage = monthUsage match {
+			case x if x > t2max => t2max - t1max
+			case x if x < t1max => 0
+			case _ => monthUsage - t1max
+		}
+		val t3Usage = monthUsage match {
+			case x if x > t3max => t3max - t2max
+			case x if x < t2max => 0
+			case _ => monthUsage - t2max
+		}
+		val t4Usage = monthUsage match {
+			case x if x > t3max => x - t3max
+			case x if x < t3max => 0
+			case _ => monthUsage - t3max
+		}
+		
+		val t1Amt = t1Usage * pricesByTierSummer(1) / 100
+		val t2Amt = t2Usage * pricesByTierSummer(2) / 100
+		val t3Amt = t3Usage * pricesByTierSummer(3) / 100
+		val t4Amt = t4Usage * pricesByTierSummer(4) / 100
+		val totalUsage = t1Usage + t2Usage + t3Usage + t4Usage
+		val totalAmt = t1Amt + t2Amt + t3Amt + t4Amt
+		List(totalUsage, totalAmt, t1Usage, t1Amt, t2Usage, t2Amt, t3Usage, t3Amt, t4Usage, t4Amt)
+	}
+	
+	def getDR2Bills() = {
+        months.map(monthID => 
+            getMonthlyBillDR2(getMonthUsage(monthID))
+        )
+    }
+	
+    val billsDR2 = getDR2Bills
+    val billTotalsDR2 = billsDR2.map(x => x(1))
+    val avgBillAmtDR2 = billTotalsDR2.sum / billTotalsDR2.length
+    val totalAmtDR2 = billTotalsDR2.sum
 
 
     def getBillEVTOU2(hourlyUsage: List[Int]) = {
@@ -133,9 +184,12 @@ case class Usage (custID:Int) {
             getMonthlyTotalEVTOU2(monthID)
         )
     }
-    val bills = getEVTOU2Bills
-    val billTotals = bills.map(x => x(0))
-    val avgBillAmt = billTotals.sum / billTotals.length
-    val totalAmt = billTotals.sum
+    val billsEVTOU2 = getEVTOU2Bills
+    val billTotalsEVTOU2 = billsEVTOU2.map(x => x(0))
+    val avgBillAmtEVTOU2 = billTotalsEVTOU2.sum / billTotalsEVTOU2.length
+    val totalAmtEVTOU2 = billTotalsEVTOU2.sum
+	
+	
+	val savingsTOU = totalAmtDR2 - totalAmtEVTOU2
 
 }
